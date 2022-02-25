9a938e827f3b6c5853e5a0dd6a888f6fadfb259d
Jack Leslie <jack.leslie@transferwise.com>
2022-02-25 15:34:16 +0000

    encode content

--- src/bundle-output-file.ts
+++ src/bundle-output-file.ts
@@ -1,4 +1,5 @@
 import * as github from '@actions/github';
+import { Base64 } from 'js-base64';
 
 import type { Octokit } from './types';
 
@@ -7,10 +8,11 @@ export async function createBundleOutputFile(
   body: string,
 ): Promise<void> {
   console.log('Updating OUTPUT.md file with latest bundle sizes');
+  const content = Base64.encode(body);
   const response = await octokit.rest.repos.createOrUpdateFileContents({
     ...github.context.repo,
     path: "OUTPUT.md",
-    content: body,
+    content,
     message: "docs: updating OUTPUT.md with latest bundle sizes",
   });
   console.log(`Done with status ${response.status}`);
