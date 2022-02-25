cef33762cf0c1559773b13707420ace5bb1d91a8
Jack Leslie <jack.leslie@transferwise.com>
2022-02-25 15:22:42 +0000

    update dist

--- dist/index.js
+++ dist/index.js
@@ -15352,7 +15352,7 @@ function getSign(bytes) {
     return bytes < 0 ? '-' : '+';
 }
 
-;// CONCATENATED MODULE: ./src/comments.ts
+;// CONCATENATED MODULE: ./src/bundle-output-file.ts
 var __assign = (undefined && undefined.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -15401,25 +15401,91 @@ var __generator = (undefined && undefined.__generator) || function (thisArg, bod
     }
 };
 
-function createOrReplaceComment(octokit, issueNumber, searchString, body) {
+function createBundleOutputFile(octokit, body) {
     return __awaiter(this, void 0, void 0, function () {
-        var comments, existing, response, response;
+        var response;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, octokit.rest.issues.listComments(__assign(__assign({}, github.context.repo), { issue_number: issueNumber }))];
+                case 0:
+                    console.log('Updating OUTPUT.md file with latest bundle sizes');
+                    return [4 /*yield*/, octokit.rest.repos.createOrUpdateFileContents(__assign(__assign({}, github.context.repo), { path: "OUTPUT.md", content: body, message: "docs: updating OUTPUT.md with latest bundle sizes" }))];
+                case 1:
+                    response = _a.sent();
+                    console.log("Done with status " + response.status);
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+
+;// CONCATENATED MODULE: ./src/comments.ts
+var comments_assign = (undefined && undefined.__assign) || function () {
+    comments_assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return comments_assign.apply(this, arguments);
+};
+var comments_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var comments_generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+
+function createOrReplaceComment(octokit, issueNumber, searchString, body) {
+    return comments_awaiter(this, void 0, void 0, function () {
+        var comments, existing, response, response;
+        return comments_generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, octokit.rest.issues.listComments(comments_assign(comments_assign({}, github.context.repo), { issue_number: issueNumber }))];
                 case 1:
                     comments = _a.sent();
                     existing = comments.data.find(function (comment) { return comment.body && comment.body.includes(searchString); });
                     if (!existing) return [3 /*break*/, 3];
                     console.log("Updating comment " + existing.id);
-                    return [4 /*yield*/, octokit.rest.issues.updateComment(__assign(__assign({}, github.context.repo), { comment_id: existing.id, body: body }))];
+                    return [4 /*yield*/, octokit.rest.issues.updateComment(comments_assign(comments_assign({}, github.context.repo), { comment_id: existing.id, body: body }))];
                 case 2:
                     response = _a.sent();
                     console.log("Done with status " + response.status);
                     return [3 /*break*/, 5];
                 case 3:
                     console.log("Creating comment on PR " + issueNumber);
-                    return [4 /*yield*/, octokit.rest.issues.createComment(__assign(__assign({}, github.context.repo), { issue_number: issueNumber, body: body }))];
+                    return [4 /*yield*/, octokit.rest.issues.createComment(comments_assign(comments_assign({}, github.context.repo), { issue_number: issueNumber, body: body }))];
                 case 4:
                     response = _a.sent();
                     console.log("Done with status " + response.status);
@@ -15646,6 +15712,7 @@ var src_generator = (undefined && undefined.__generator) || function (thisArg, b
 
 
 
+
 var ARTIFACT_NAME = 'next-bundle-analyzer';
 var FILE_NAME = 'bundle-sizes.json';
 var DYNAMIC_FILE_NAME = 'dynamic-bundle-sizes.json';
@@ -15694,6 +15761,7 @@ function run() {
                             (routesTable + "\n\n") +
                             (dynamicTable + "\n\n");
                         createOrReplaceComment(octokit, issueNumber, prefix, body);
+                        createBundleOutputFile(octokit, body);
                     }
                     return [3 /*break*/, 6];
                 case 5:
